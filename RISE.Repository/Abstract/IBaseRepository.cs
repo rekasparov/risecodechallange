a74d1f6e4dc2c7d1c79c6d379dc18a2fd37ed5f3
mustafa burak kayabal <mburakkayabal@hotmail.com>
2021-10-23 12:23:57 +0300

    Fixes #3

---
+++ RISE.Repository/Abstract/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RISE.Repository.Abstract
+{
+    public interface IBaseRepository<T>
+        where T : class, new()
+    {
+        IQueryable<T> Select(Expression<Func<T, bool>> predicate = null);
+        void Insert(T enitity);
+        void Update(T enitity);
+        void Delete(T enitity);
+    }
+}
---
+++ RISE.Repository/Concrete/BaseRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RISE.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RISE.Repository.Concrete
+{
+    public class BaseRepository<T> : IBaseRepository<T>
+        where T : class, new()
+    {
+        protected DbContext dbContext;
+
+        public BaseRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Delete(T enitity)
+        {
+            try
+            {
+                dbContext.Remove(enitity).State = EntityState.Deleted;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void Insert(T enitity)
+        {
+            try
+            {
+                dbContext.Add(enitity).State = EntityState.Added;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public IQueryable<T> Select(Expression<Func<T, bool>> predicate = null)
+        {
+            try
+            {
+                if (predicate != null) return dbContext.Set<T>().Where(predicate);
+
+                return dbContext.Set<T>().AsQueryable();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void Update(T enitity)
+        {
+            try
+            {
+                dbContext.Update(enitity).State = EntityState.Modified;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
