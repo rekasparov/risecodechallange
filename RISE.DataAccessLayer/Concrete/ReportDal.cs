17d63bf15a60f95d139f0242182f63d1bd8d67ea
mustafa burak kayabal <mburakkayabal@hotmail.com>
2021-10-23 12:40:19 +0300

    Fixes #7

---
+++ RISE.DataAccessLayer/Concrete/ReportDal.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RISE.DataAccessLayer.Abstract;
+using RISE.Entity;
+using RISE.Repository.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RISE.DataAccessLayer.Concrete
+{
+    public class ReportDal : BaseRepository<Report>, IReportDal
+    {
+        public ReportDal(DbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+    }
+}
