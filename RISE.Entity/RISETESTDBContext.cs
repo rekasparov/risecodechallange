452eb61649711cdbb7fecef181b2db4a2be89150
mustafa burak kayabal <mburakkayabal@hotmail.com>
2021-10-23 12:28:04 +0300

    Fixes #5

---
+++ RISE.Entity/RISETESTDBContext.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace RISE.Entity
+{
+    public partial class RISETESTDBContext : DbContext
+    {
+        public RISETESTDBContext()
+        {
+        }
+
+        public RISETESTDBContext(DbContextOptions<RISETESTDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ContactType> ContactTypes { get; set; }
+        public virtual DbSet<Person> People { get; set; }
+        public virtual DbSet<PersonContact> PersonContacts { get; set; }
+        public virtual DbSet<Report> Reports { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=RISETESTDB;User Id=sa;Password=07081983Konya");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Turkish_CI_AS");
+
+            modelBuilder.Entity<ContactType>(entity =>
+            {
+                entity.HasKey(e => e.UUID);
+
+                entity.ToTable("ContactType");
+
+                entity.Property(e => e.UUID).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.HasKey(e => e.UUID);
+
+                entity.ToTable("Person");
+
+                entity.Property(e => e.UUID).ValueGeneratedNever();
+
+                entity.Property(e => e.Company)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Surname)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<PersonContact>(entity =>
+            {
+                entity.HasKey(e => new { e.PersonId, e.ContactTypeId });
+
+                entity.ToTable("PersonContact");
+
+                entity.Property(e => e.Content)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.ContactType)
+                    .WithMany(p => p.PersonContacts)
+                    .HasForeignKey(d => d.ContactTypeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.Person)
+                    .WithMany(p => p.PersonContacts)
+                    .HasForeignKey(d => d.PersonId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            modelBuilder.Entity<Report>(entity =>
+            {
+                entity.HasKey(e => e.UUID);
+
+                entity.ToTable("Report");
+
+                entity.Property(e => e.UUID).ValueGeneratedNever();
+
+                entity.Property(e => e.RequestDate).HasColumnType("datetime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ RISE.Entity/Report.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace RISE.Entity
+{
+    public partial class Report
+    {
+        public Guid UUID { get; set; }
+        public DateTime RequestDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
